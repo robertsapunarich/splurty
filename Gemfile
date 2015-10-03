d17943c376fb64b9e3e6a98d2e2c212efa9995f5
Robert Sapunarich <robertsapunarich@gmail.comgit config --global user.email robertsapunarich@gmail.com>
2015-10-03 22:16:02 +0000

    added simple_form and links

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ end
 # include bootstrap
 gem 'bootstrap-sass', '~> 3.3.4'
 
+gem 'simple_form'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.1.2'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Splurty::Application.routes.draw do
+  root 'quotes#index'
 
-root 'quotes#index'
+  resources :quotes
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
