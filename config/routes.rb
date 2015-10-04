af5fb2dc470a7edf93a27a0afeb60a2ce6029a16
Robert Sapunarich <robertsapunarich@gmail.comgit config --global user.email robertsapunarich@gmail.com>
2015-10-04 18:28:05 +0000

    created about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Splurty::Application.routes.draw do
   root 'quotes#index'
-
   resources :quotes
+  get 'about', :to => 'quotes#about'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
