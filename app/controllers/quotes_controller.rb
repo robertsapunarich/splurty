b882eab57ead714bf80d4c463b761319a44ca3e9
Robert Sapunarich <robertsapunarich@gmail.comgit config --global user.email robertsapunarich@gmail.com>
2015-10-04 00:05:52 +0000

    fixed error in quotes_controller.rb by defining @quote variable

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -8,7 +8,7 @@ class QuotesController < ApplicationController
 	end
 
 	def create
-		Quote.create(quote_params)
+		@quote = Quote.create(quote_params)
 		if @quote.invalid?
 			flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
 		end
