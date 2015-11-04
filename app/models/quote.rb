433c3061c9badf0606f65546d1ab256d6c856276
Robert Sapunarich <robertsapunarich@gmail.comgit config --global user.email robertsapunarich@gmail.com>
2015-11-04 00:09:52 +0000

    added unique_tag test to quote_test.rb that uses assert_equal method. added unique_tag method to Quote model that splits a quote author's name into two separate strings, then collects them into an array and grabs the 0 element from each string, and concatenates them with join. added unique_tag method to index view.

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,12 @@
 class Quote < ActiveRecord::Base
 	validates :saying, :presence => true, :length => { :maximum => 140, :minimum => 3}
 	validates :author, :presence => true, :length => { :maximum => 50, :minimum => 3}
+
+  def unique_tag
+    abbr = self.author.split(" ").collect do |sub_string|
+      sub_string[0]
+    end
+
+    abbr.join + '#' + self.id.to_s
+  end
 end
--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+
+  test "unique_tag"  do
+    quote = Quote.create(:author => 'Professor Robbie', :saying => 'Some adjuncts have more degrees than fahrenheit.')
+    expected = 'PR#' + quote.id.to_s
+    actual = quote.unique_tag
+    assert_equal expected, actual
   end
+
 end
